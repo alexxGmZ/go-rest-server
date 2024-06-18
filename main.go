cf5148c1a2374aeb058a22f39d01cdb28ffc2d24
alexxGmZ <algilbertgomez@gmail.com>
2024-06-18 12:03:42 +0800

    just did the basics or REST in GO
    followed the documentation here
    https://go.dev/doc/tutorial/web-service-gin

--- main.go
+++ main.go
@@ -4,9 +4,23 @@ import (
 	"log"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	// "github.com/lib/pq"
+	"net/http"
 )
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
 
 func main() {
 	err := godotenv.Load()
@@ -17,4 +31,26 @@ func main() {
 	postgresConnURL := os.Getenv("POSTGRESURL")
 
 	log.Println(postgresConnURL)
+
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumById)
+
+	router.Run("localhost:8080")
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func getAlbumById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
