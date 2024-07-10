125361f72acd714ec559cb0e014b781c9863eecd
alexxGmZ <algilbertgomez@gmail.com>
2024-07-10 21:37:52 +0800

    feat(delete): apply taskId database verification

--- api/delete.go
+++ api/delete.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"go_rest/utils"
@@ -10,14 +11,28 @@ import (
 // Deletes a task from the database by its ID. Requires the "taskId" parameter in the
 // endpoint. It responds with a success message or an error message in case of a failure.
 func DeleteTask(c *gin.Context) {
-	taskId := c.Param("taskId")
+	taskIdStr := c.Param("taskId")
+   taskIdInt, err := strconv.Atoi(taskIdStr)
 
 	sqlQuery := `
 		DELETE FROM Tasks
 		WHERE task_id = $1
 	`
 
-	_, err := utils.DB.Exec(sqlQuery, taskId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to convert int to string",
+			"error":   err,
+		})
+		return
+	}
+
+	if !utils.VerifyTask(taskIdInt) {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Task not found"})
+		return
+	}
+
+	_, err = utils.DB.Exec(sqlQuery, taskIdInt)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Failed to delete task",
